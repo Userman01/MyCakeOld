fa1de3cd7f4ed2482407266e30961475b8dc0486
Petr <postnikov_pp@albank.ru>
2022-06-27 20:12:42 +0900

    init: start project

---
+++ MyCake/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  MyCake
+//
+//  Created by Петр Постников on 16.06.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ MyCake/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MyCake
+//
+//  Created by Петр Постников on 16.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
